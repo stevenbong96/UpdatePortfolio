eb9d28269f71b4c98c3f23b23950cf3a3576faf6
Steven Bong <stevenseal99@yahoo.com>
2020-09-28 13:50:18 -0700

    Create basic files. Add portfolio page. Start laying the UI layout

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Add dropdown
+    $('select').formSelect();
+
+    // Add sidenav to become mobile responsive
+    $('.sidenav').sidenav();
+
+    // Add sticky navbar
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $("nav").addClass("stickynav");
+        } else {
+            $("nav").removeClass("stickynav");
+        }
+    })
+
+    // Add tooltips
+    $('.tooltipped').tooltip();
+});
